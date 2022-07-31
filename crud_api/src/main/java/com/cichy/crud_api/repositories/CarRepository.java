fdd0cce8986a199df2edcb5e1db7949494940335
Cichy50 <szymoncichulski@gmail.com>
2022-07-31 15:49:59 +0200

    added @Repository annotation

--- crud_api/src/main/java/com/cichy/crud_api/repositories/CarRepository.java
+++ crud_api/src/main/java/com/cichy/crud_api/repositories/CarRepository.java
@@ -2,6 +2,8 @@ package com.cichy.crud_api.repositories;
 
 import com.cichy.crud_api.domain.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CarRepository extends JpaRepository<Car,Long> {
 }
--- crud_api/src/main/java/com/cichy/crud_api/repositories/OwnerRepository.java
+++ crud_api/src/main/java/com/cichy/crud_api/repositories/OwnerRepository.java
@@ -2,6 +2,8 @@ package com.cichy.crud_api.repositories;
 
 import com.cichy.crud_api.domain.Owner;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OwnerRepository extends JpaRepository<Owner, Long> {
 }
